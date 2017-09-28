a42a2d15e392df7a131e585d3cc04fac42cc9acc
amansidhant <31943078+amansidhant@users.noreply.github.com>
2017-09-27 21:58:02 -0700

    Add files via upload

--- foobar.rb
+++ foobar.rb
@@ -1,9 +1,14 @@
 class Foobar
-
-  def self.baz(a)
+    
+    def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
-  end
+    a=a.map(|a| a.to_f)
+    a=a.map(|a| a+=2)
+    a=a.delete_if {|a| a%2!=0}
+    a=a.delete_if {|a| a>10}
+    a.inject(0){|sum,x| sum + x}
+end
 end
 
 
